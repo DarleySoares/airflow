8c3a0ffbd5f09731737a10709f86d43aa2a924f1
Darley R Soares <darleysoares27@gmail.com>
2023-10-25 22:01:40 -0300

    atualização de dags

---
+++ docker/dags/dag1.py
@@ -0,0 +1,47 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+from typing import Dict
+
+default_args: Dict[str, str] = {
+    "retries": 1,
+    "retry_delay": timedelta(minutes = 5)
+}
+
+def hello_world() -> str:
+    return "Hello World!"
+
+def good_morning(ds, **kwargs) -> str:
+    return f"Hi, {kwargs['user']}! Good morning, today is {ds}"
+
+dag = DAG(
+    dag_id="dag_1",
+    start_date=datetime(2023, 1, 1),
+    schedule="0 9 * * *",
+    default_args=default_args,
+    catchup=False,
+    tags=["good_morning", "daily"]
+)
+
+task_1 = BashOperator(
+    task_id="task_1",
+    bash_command="echo 1",
+    dag=dag
+)
+
+task_2 = PythonOperator(
+    task_id="task_2",
+    python_callable=hello_world, 
+    dag=dag
+)
+
+task_3 = PythonOperator(
+    task_id="task_3",
+    python_callable=good_morning,
+    provide_context=True,
+    op_kwargs={"user": "Rodolfo"},
+    dag=dag
+)
+    
+task_1 >> task_2 >> task_3
---
+++ docker/dags/dag4.py
@@ -0,0 +1,101 @@
+from airflow.decorators import dag
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from datetime import datetime, timedelta
+from typing import Dict
+
+default_args: Dict[str, str] = {
+    "retries": 1,
+    "retry_delay": timedelta(minutes = 5)
+}
+
+def random_hour(ti) -> int:
+    from random import randint
+    hour = randint(0, 24)
+    ti.xcom_push(key="hour", value = hour)
+    return hour
+
+def show_hour(ts, **kwargs) -> str:
+    return f"SYSTEM HOUR: {ts} | RANDOM HOUR: {kwargs['hour']}"
+
+
+def branching(ti):
+    hour = ti.xcom_pull(task_ids=['random_hour'], key='hour')
+    
+    if hour >= 0 and hour < 12:
+        return "good_morning"
+    elif hour >= 12 and hour < 18:
+        return "good_afternoon"
+    elif hour >= 18:
+        return "good_night"
+    
+
+def good_morning(ds, **kwargs) -> str:
+    return f"Hi, {kwargs['user']}! Good morning, today is {ds}"
+
+
+def good_afternoon(ds, **kwargs) -> str:
+    return f"Hi, {kwargs['user']}! Good afternoon, today is {ds}"
+
+
+def good_night(ds, **kwargs) -> str:
+    return f"Hi, {kwargs['user']}! Good night, today is {ds}"
+
+@dag(
+    dag_id = "dag_xcom",
+    start_date = datetime(2023, 10, 20),
+    schedule = "0 21 * * *",
+    default_args = default_args,
+    catchup = True,
+    max_active_runs=1,
+    max_active_tasks=1,
+    tags=["XCOM", "daily"]
+)
+def generate_dag():
+    
+    
+    task_1 = PythonOperator(
+        task_id="random_hour",
+        python_callable=random_hour,
+        provide_context=True,
+    )
+    
+    task_2 = PythonOperator(
+        task_id="show_hour",
+        python_callable=show_hour,
+        provide_context=True,
+        op_kwargs={"hour": "{{ ti.xcom_pull(task_ids=['random_hour'], key='hour')[0] }}"}
+    )
+    
+    task_3 = BranchPythonOperator(
+        task_id="branching",
+        python_callable=branching,
+        provide_context=True,
+        op_kwargs={
+            "hour": "{{ ti.xcom_pull(task_ids=['random_hour'], key='hour')[0] }}"}
+    )
+    
+    option_1 = PythonOperator(
+        task_id="good_morning",
+        python_callable=good_morning,
+        provide_context=True,
+        op_kwargs={"user": "Rodolfo", "dt_ref": "{{ ds }}"}
+    )
+    
+    option_2 = PythonOperator(
+        task_id="good_afternoon",
+        python_callable=good_afternoon,
+        provide_context=True,
+        op_kwargs={"user": "Rodolfo", "dt_ref": "{{ ds }}"}
+    )
+    
+    option_3 = PythonOperator(
+        task_id="good_night",
+        python_callable=good_night,
+        provide_context=True,
+        op_kwargs={"user": "Rodolfo", "dt_ref": "{{ ds }}"}
+    )
+    
+    task_1 >> task_2 >> task_3 >> [option_1, option_2, option_3]
+
+ 
+generate_dag()
